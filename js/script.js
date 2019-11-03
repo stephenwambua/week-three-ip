07b0863c3389a52ac40790beed568d1ac1ac0986
stephen wambua <ngumbausteph@gmail.com>
2019-11-03 22:11:11 +0300

    add a a click and togol method to the dev and prod id

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $('#dsn').click(function(){
-$('#dsn p').toggle();
-$('#dsn img').toggle();
+    $('#dsn p').toggle();
+    $('#dsn img').toggle();
+});
+$('#dev').click(function(){
+    $('#dev p').toggle();
+    $('#dev img').toggle();
+});
+$('#prod').click(function(){
+    $('#prod p').toggle();
+    $('#prod img').toggle();
 });
