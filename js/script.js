c8fbf06b538da8407c65a9321eaa0a24794d697c
stephen wambua <ngumbausteph@gmail.com>
2019-11-04 13:20:49 +0300

    add scripts to provide hover effect on the portfolios in the html file

--- js/script.js
+++ js/script.js
@@ -10,3 +10,51 @@ $('#prod').click(function(){
     $('#prod p').toggle();
     $('#prod img').toggle();
 });
+$('#firststep').mouseenter(function(){
+    $('#firststep p').show();
+});
+$('#firststep').mouseleave(function(){
+    $('#firststep p').hide();
+});
+$('#steptwo').mouseenter(function(){
+    $('#steptwo p').show();
+});
+$('#steptwo').mouseleave(function(){
+    $('#steptwo p').hide();
+});
+$('#stepthree').mouseenter(function(){
+    $('#stepthree p').show();
+});
+$('#stepthree').mouseleave(function(){
+    $('#stepthree p').hide();
+});
+$('#stepfour').mouseenter(function(){
+    $('#stepfour p').show();
+});
+$('#stepfour').mouseleave(function(){
+    $('#stepfour p').hide();
+});
+$('#stepfive').mouseenter(function(){
+    $('#stepfive p').show();
+});
+$('#stepfive').mouseleave(function(){
+    $('#stepfive p').hide();
+});
+$('#stepsix').mouseenter(function(){
+    $('#stepsix p').show();
+});
+$('#stepfour').mouseleave(function(){
+    $('#stepsix p').hide();
+});
+$('#stepseven').mouseenter(function(){
+    $('#stepseven p').show();
+});
+$('#stepseven').mouseleave(function(){
+    $('#stepseven p').hide();
+});
+$('#stepeight').mouseenter(function(){
+    $('#stepeight p').show();
+});
+$('#stepeight').mouseleave(function(){
+    $('#stepeight p').hide();
+});
